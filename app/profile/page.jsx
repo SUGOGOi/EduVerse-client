c9ede1734dd20386b1448a54ac25532e45fb2c11
Sumsum Gogoi <sumsumgogoi51@gmail.com>
2024-04-29 06:59:59 +0530

    check error

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,32 +6,44 @@ import { useDispatch, useSelector } from "react-redux"
 import qr from "../../public/images/qr.png"
 import { useGetMyProfileQuery } from "@/redux/apis/userApi"
 import { loadUserReducer } from '@/redux/reducers/userReducer'
+import Navbar from '@/components/navbar/Navbar'
+import { toast } from "react-hot-toast";
+
 
 const Page = () => {
-    const { data, isLoading } = useGetMyProfileQuery();
+    const { data, isLoading, error } = useGetMyProfileQuery();
     const { user } = useSelector(state => state.userReducer);
     const dispatch = useDispatch();
 
 
-    console.log(user)
 
-    useEffect(() => {
-        if (data) {
-            dispatch(loadUserReducer(data))
-        }
-    }, [data])
-
-
+    if (data) {
+        toast.success(data.message)
+        dispatch(loadUserReducer(data))
+    }
+    if (error) {
+        // console.log(error)
+        const err = error;
+        const messageRes = err.data.error;
+        toast.error(messageRes)
+    }
 
     return (
-        <div className={style.container} >
-            <div className={style.myDetails}>
-                <Image src={qr} alt="" width={200} height={200} />
-                <div className={style.name}></div>
-                <div className={style.email}></div>
+        <>
+            <Navbar />
+            <div className={style.container} >
+                <div className={style.profileCon}>
+                    <div className={style.myDetails}>
+                        <Image src={qr} alt="" width={200} height={200} />
+                        <div className={style.name}></div>
+                        <div className={style.email}></div>
+                    </div>
+                    <div className={style.myCourses}>
+
+                    </div>
+                </div>
             </div>
-            <div className={style.myCourses}></div>
-        </div>
+        </>
     )
 }
 
