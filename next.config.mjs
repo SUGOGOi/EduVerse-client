99ce30327de0194af67d11065dfc0ded55b5b01a
Sumsum Gogoi <sumsumgogoi51@gmail.com>
2024-05-17 08:34:49 +0530

    sss

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     // SERVER: "http://localhost:8000/",
-    SERVER: "https://eduverse-server-d6nq.onrender.com/",
+    SERVER: "https://eduverse-server-jii8.onrender.com/",
     JWT_SECRET:
       "00qsgWvy7rjiwsAHAZTgei6opIxX2ObpjsH7u20dyIyW/6FN7Sswvkl2u0kHRzLG",
   },
