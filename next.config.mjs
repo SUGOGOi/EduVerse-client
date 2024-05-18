ecdf9d47285f30596d82b2b729983654ec902f1d
Sumsum Gogoi <sumsumgogoi51@gmail.com>
2024-05-18 14:08:28 +0530

    mmmmmmm

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    SERVER: "http://localhost:8000/",
-    // SERVER: "https://eduverse-server-jii8.onrender.com/",
+    // SERVER: "http://localhost:8000/",
+    SERVER: "https://eduverse-server-jii8.onrender.com/",
   },
   images: {
     remotePatterns: [
       {
-        hostname: "localhost",
-        // hostname: "eduverse-server-d6nq",
+        // hostname: "localhost",
+        hostname: "eduverse-server-d6nq",
       },
     ],
   },
